c28ca9656b75e593d5c743a9d9fd0fbe1d82001a
kelril <kelril@dsu1.service.sarnet>
2020-02-12 22:28:47 +0300

    0.1

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,107 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <cstdlib>
+#include <iostream>
+#include <new>
+
+using std::cout;
+using std::endl;
+
+template <typename T>
+class stack
+{
+private:
+    T* arrStack;    // Динамический масив-указатель на стек
+    int count;      // Вершина стека - количество элементов типа T в стеке
+
+public:
+
+    stack() {       // конструктор
+        arrStack = nullptr;
+        count = 0;  // количество элементов в стеке определяется по значению count
+    }
+
+
+    void push(T &&value)    // Помещаем элемент в стек
+    {
+        T* tmp; // временный указатель
+        // блок try необходим для перехвата исключения, если память не выделится
+        try {
+            tmp = arrStack; // указатель указывает на arrStack
+            arrStack = new T[count + 1];    // выделить память на 1 элемент больше, чем было выделено до этого
+            count++;    // увеличить количество элементов в стеке на 1
+
+            for (int i = 0; i < count - 1; i++) {
+                arrStack[i] = tmp[i];   // скопировать данные из памяти, на которую указывает tmp в память, на которую указывает arrStack
+            }
+
+            arrStack[count - 1] = value;    // добавить последний элемент
+
+            // освободить память, выделенную перед этим для arrStack,
+            // на эту память указывает tmp
+            if (count > 1)
+                delete[] tmp;
+        }
+        catch (bad_alloc e)
+        {
+            // если память не выделилась
+            cout << e.what() << endl;
+        }
+    }
+
+    void push(const T &value)
+    {
+        T* tmp; // временный указатель
+        // блок try необходим для перехвата исключения, если память не выделится
+        try {
+            tmp = arrStack; // указатель указывает на arrStack
+            arrStack = new T[count + 1];    // выделить память на 1 элемент больше, чем было выделено до этого
+            count++;    // увеличить количество элементов в стеке на 1
+
+            for (int i = 0; i < count - 1; i++) {
+                arrStack[i] = tmp[i];   // скопировать данные из памяти, на которую указывает tmp в память, на которую указывает arrStack
+            }
+
+            arrStack[count - 1] = value;    // добавить последний элемент
+
+            // освободить память, выделенную перед этим для arrStack,
+            // на эту память указывает tmp
+            if (count > 1)
+                delete[] tmp;
+        }
+        catch (bad_alloc e)
+        {
+            // если память не выделилась
+            cout << e.what() << endl;
+        }
+    }
+
+
+    // Вытащить элемент из стека (запопопать)
+    // При вытягивании элемента из стека память не переопределяется
+    T pop() {
+        if (count == 0)
+            return 0; // стек пуст
+        count--;
+        return arrStack[count];
+    }
+
+    // Просмотр элемента в вершине стека
+    const T &head() const
+    {
+        if (count == 0)
+            return 0;
+        return arrStack[count - 1];
+    }
+
+    // Деструктор - освобождает память
+    ~stack()
+    {
+        if (count > 0)
+            delete[] arrStack;
+    }
+
+};
+
 #endif // INCLUDE_HEADER_HPP_
