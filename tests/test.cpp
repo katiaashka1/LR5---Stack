436952fbe754162ac0513743261d5a1a1d20461d
kelril <kelril@dsu1.service.sarnet>
2020-02-16 02:45:31 +0300

    0.3.1

--- tests/test.cpp
+++ tests/test.cpp
@@ -33,26 +33,26 @@ EXPECT_EQ(test.pop(), 3);
 EXPECT_EQ(test.pop(), 2);
 }
 
-// TEST(stack_t, Text){
-//     stack_st<summ> test_summ;
-//     stack_st<int> test;
-//     test_summ.push_emplace(1, 2, 3);
-//     test_summ.push_emplace(3, 4, 5);
-//     test.push(2);
-//     test.push(3);
-//     test.push(4);
-//     test.push(12);
-//     EXPECT_EQ(std::is_move_assignable<stack_st<int>>::value, true);
-//     EXPECT_EQ(std::is_move_constructible<stack_st<int>>::value, true);
+TEST(stack_t, Text){
+    stack_st<summ> test_summ;
+    stack_st<int> test;
+    test_summ.push_emplace(1);
+    test_summ.push_emplace(3);
+    test.push(2);
+    test.push(3);
+    test.push(4);
+    test.push(12);
+    EXPECT_EQ(std::is_move_assignable<stack_st<int>>::value, true);
+    EXPECT_EQ(std::is_move_constructible<stack_st<int>>::value, true);
 
-//     EXPECT_EQ(test.head(), 12);
-//     EXPECT_EQ(test.pop(), 12);
-//     EXPECT_EQ(test.pop(), 4);
-//     EXPECT_EQ(test.pop(), 3);
-//     EXPECT_EQ(test.pop(), 2);
-//     EXPECT_EQ(test_summ.pop().rez, 12);
-//     EXPECT_EQ(test_summ.pop().rez, 6);
-// }
+    EXPECT_EQ(test.head(), 12);
+    EXPECT_EQ(test.pop(), 12);
+    EXPECT_EQ(test.pop(), 4);
+    EXPECT_EQ(test.pop(), 3);
+    EXPECT_EQ(test.pop(), 2);
+    EXPECT_EQ(test_summ.pop().rez, 12);
+    EXPECT_EQ(test_summ.pop().rez, 6);
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
